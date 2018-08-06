08545e87c87d084bf5bcd7232c39edaa161b94f5
Catman2989 <36837549+Catman2989@users.noreply.github.com>
2018-08-06 16:29:34 +1000

    Fibonacci start

--- week2/week2/Program.cs
+++ week2/week2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +11,28 @@ namespace week2
         static void Main(string[] args)
         {
             bool notPrime = false;
-            int halfNumber = number / 2;
+            int primesFound = 0;
+            
+            Console.WriteLine("enter number number of primes");
+            string strenterd = Console.ReadLine();
+            int.TryParse(strenterd, int numOfPrime);
+            int number1 = 1;
+            int number2 = 1;
 
+
+
+            int halfNumber = number1 / 2;
             for (int i = 0; i == halfNumber || notPrime = true; i++)
             {
                 i % halfNumber = int checkPrime;
-
+                if(checkPrime == 0)
+                {
+                    return notPrime = true;
+                }
+                else
+                {
+                    return notPrime = false;
+                }
             }
         }
     }
