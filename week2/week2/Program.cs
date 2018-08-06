57dd918365b4c9ba302754d375107575bb91f150
MATTHEW RONZIO <102048675@student.swin.edu.au>
2018-08-06 15:13:07 +1000

    initial commit

---
+++ week2/week2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool notPrime = false;
+            int halfNumber = number / 2;
+
+            for (int i = 0; i == halfNumber || notPrime = true; i++)
+            {
+                i % halfNumber = int checkPrime;
+
+            }
+        }
+    }
+}
